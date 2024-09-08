3a1d21764baac94fda8f0a375141f7ddd7a32e33
chiacn <chiacn@naver.com>
2024-09-09 00:33:38 +0900

    ui: MobileNavigationBar 추가

--- src/components/layout/CommonContainer.tsx
+++ src/components/layout/CommonContainer.tsx
@@ -1,14 +1,21 @@
+"use client";
 import NavigationBar from "./navbar/NavigationBar";
+import MobileNavigationBar from "./navbar/MobileNavigationBar";
+import useIsMobile from "./hooks/useIsMobile";
 
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 export default function CommonContainer({ children }: ContainerProps) {
+  const isMobile = useIsMobile({ maxWidth: 640 });
+  if (isMobile === null) {
+    return null; // Note: 모바일 여부 판단이 안 되었을 시 표시 x
+  }
   return (
     <>
-      <NavigationBar />
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      {isMobile ? <MobileNavigationBar /> : <NavigationBar />}
+      <div className="flex min-h-screen flex-col items-center justify-between">
         {children}
       </div>
     </>
