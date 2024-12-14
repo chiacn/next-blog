1bc2e9523c893e2559e1cffb7d673fb9dca9bc38
chiacn <chiacn@naver.com>
2024-12-14 17:59:15 +0900

    fix: next.config.mjs - basePath 변경

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export", // Next 14 버전에서는 next exprot 명령 대신 output: "export"
-  basePath: isProduction ? "/text-to-diagram" : "",
+  basePath: isProduction ? "/next-blog" : "",
   // next 이미지 최적화 옵션 off
   // Note:  Image 태그를 사용하게 되면 이미지 랜더링 시에 Next에서 자동적으로 이미지 최적화를 하는데 해당 기능은 정적 파일만을 사용하는 SSG 방식에서는 불가능하므로 unoptimized 옵션을 true로 하여 Next 이미지 최적화 옵션을 꺼야한다.
   images: {
