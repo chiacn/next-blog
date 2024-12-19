257ecc929cdbe4dae6cb9ef6562994d6041cca6f
chiacn <chiacn@naver.com>
2024-12-19 22:22:53 +0900

    post 파일 정리

--- src/posts/path/articles.ts
+++ src/posts/path/articles.ts
@@ -1,28 +1,17 @@
 export const articlesPath = [
   // directory -----------------------------------------
-  { slug: ["articles", "Argorithm", "Test"] },
-  { slug: ["articles", "example", "CS"] },
-  { slug: ["articles", "example", "JavaScript"] },
-  { slug: ["articles", "test"] },
+  { slug: ["articles", "JavaScript"] },
+  { slug: ["articles", "Front"] },
+  { slug: ["articles", "CS"] },
   { slug: ["blog", "회고"] },
-  { slug: ["blog", "blog", "회고"] },
 
   // mdx -----------------------------------------------
   {
-    slug: ["articles", "example", "JavaScript", "Event Loop의 이해.mdx"],
+    slug: ["articles", "JavaScript", "Event Loop의 이해.mdx"],
   },
   {
-    slug: ["articles", "example", "CS", "CORS의 이해.mdx"],
+    slug: ["articles", "CS", "CORS의 이해.mdx"],
   },
-
-  {
-    slug: ["articles", "Argorithm", "Test", "article-01.mdx"],
-  },
-  {
-    slug: ["articles", "Argorithm", "article-01.mdx"],
-  },
-
-  // blog ----------------------------
   {
     slug: ["blog", "회고", "Event Loop의 이해.mdx"],
   },
