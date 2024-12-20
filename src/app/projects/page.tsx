f9c24ccf6b9b77084b1f2c9930879efdb4aa40b0
chiacn <chiacn@naver.com>
2024-12-20 14:29:52 +0900

    add: projects 추가

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,30 +1,7 @@
 "use client";
 import CommonLineDivider from "@/components/CommonLineDivider";
+import { projectData } from "@/data/project";
 import React from "react";
-
-// 프로젝트 데이터 예시
-const projectData = [
-  {
-    title: "Text to Diagram",
-    date: "",
-    link: "https://textdiagram.com/",
-    description:
-      "주어진 text의 효율적인 이해를 위해, LLM을 이용하여 입력된 TEXT를 각각 tree, example, logical progression의 형태로 제공하는 서비스",
-    sub_description: [],
-    techStack: "React | Typescript | Next | tailwind | Langchain.js",
-  },
-  {
-    title: "Save your time",
-    date: "22.11 - 23.01",
-    link: "https://chiacn.github.io/saveyourtime/home",
-    description: "효율적인 자습을 위한 시간 관리 타이머",
-    sub_description: [
-      "React의 기본적인 상태관리를 이해하기 위한 useEffect, useState, useRef, useReducer 등 기본 Hook 사용",
-      "React 환경에서 setInterval 사용의 문제점을 다룬 Article 내용 실습\n( https://overreacted.io/making-setinterval-declarative-with-react-hooks/ )",
-    ],
-    techStack: "React, TypeScript",
-  },
-];
 
 const goToLink = (link: string) => {
   if (link && link !== "") {
@@ -75,7 +52,7 @@ export default function Page() {
             </section>
 
             {/* 프로젝트 설명 */}
-            <section className="mt-2 text-gray-600 font-light text-sm">
+            <section className="mt-2 text-gray-600 font-light text-sm whitespace-pre-line">
               <p>{project.description}</p>
               <ul
                 className="mt-4 ml-4 whitespace-pre-line"
---
+++ src/data/project.ts
@@ -0,0 +1,31 @@
+export const projectData = [
+  {
+    title: "Text to Diagram",
+    date: "",
+    link: "https://textdiagram.com/",
+    description:
+      "주어진 text의 효율적인 이해를 위해, LLM을 이용하여 입력된 TEXT를 각각 tree, example, logical progression의 형태로 제공하는 서비스",
+    sub_description: [],
+    techStack: "React | Typescript | Next | tailwind | Langchain.js",
+  },
+  {
+    title: "개인 블로그",
+    date: "",
+    link: "https://chiacn.github.io/next-blog/projects",
+    description:
+      "공부 및 트러블 슈팅 내용을 정리하기 위한 블로그 (현재 페이지) \n 이전 블로그 (2020 - 2022) - https://dadado.tistory.com/",
+    sub_description: [],
+    techStack: "React | Typescript | Next | tailwind",
+  },
+  {
+    title: "Save your time",
+    date: "22.11 - 23.01",
+    link: "https://chiacn.github.io/saveyourtime/home",
+    description: "효율적인 자습을 위한 시간 관리 타이머",
+    sub_description: [
+      "React의 기본적인 상태관리를 이해하기 위한 useEffect, useState, useRef, useReducer 등 기본 Hook 사용",
+      "React 환경에서 setInterval 사용의 문제점을 다룬 Article 내용 실습\n( https://overreacted.io/making-setinterval-declarative-with-react-hooks/ )",
+    ],
+    techStack: "React, TypeScript",
+  },
+];
